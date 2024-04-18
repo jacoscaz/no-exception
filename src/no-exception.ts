793fbe3ca3e0b04ea5a6747fa404e68c6cbd39c6
Jacopo Scazzosi <jacopo@scazzosi.com>
2024-04-18 20:46:48 +0200

    separated fail into failerr package

--- src/no-exception.ts
+++ src/no-exception.ts
@@ -10,34 +10,8 @@ const isBrowser = new Function('try { return this === window; } catch (e) { retu
 
 const toString = Object.prototype.toString;
 
-const isObject = (item: any): item is Object => {
-  return toString.call(item) === '[object Object]';
-};
-
 export const isError = (item: any): item is Error => {
   return toString.call(item) === '[object Error]';
-};
-
-// ============================================================================
-//                                    FAIL
-// ============================================================================
-
-const __fail__ = Symbol('fail');
-
-const empty = Object.freeze(Object.create(null));
-
-export interface Fail<D = {}> {
-  [__fail__]: true;
-  message: string;
-  data: D;
-}
-
-export const mkFail = <D = {}>(message: string, data: D = empty): Fail<D> => {
-  return { [__fail__]: true, message, data };
-};
-
-export const isFail = (val: any): val is Fail<any> => {
-  return isObject(val) && __fail__ in val;
 };
 
 // ============================================================================
@@ -56,7 +30,7 @@ if (isNode) {
   };
   process.once('uncaughtException', nodeHandler);
   process.once('unhandledRejection', nodeHandler);
-} 
+}
   
 if (isBrowser) {
   const browserHandler = (err: any) => {
